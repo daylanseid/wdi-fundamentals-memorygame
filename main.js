b576edcff250555a5de5c0532b316023e2d9d6f0
daylanseid <39140825+daylanseid@users.noreply.github.com>
2018-06-11 06:31:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+
+if (length === 2) {
+}
+
+var cardsInPlay = 2;
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+
+checkForMatch {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+
+flipCard {
+	var cards =
+	{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png";
+cardImage.setAttribute(src);
+console.log(cards[cardId].cardImage
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png";
+cardImage.setAttribute(src);
+console.log(cards[cardId].cardImage
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png";
+cardImage.setAttribute(src);
+console.log(cards[cardId].cardImage
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/diamonds-of-diamonds.png"
+},
+console.log(cards[cardId].cardImage)
+cardImage.setAttribute(src);
+
+var cardsInPlay = ["queen"]
+cardsInPlay.push ("cardOne");
+cardsInPlay;
+console.log("User flipped queen" = Cards[cardId].rank)
+console.log("User flipped queen" = Cards[cardId].suit)
+console.log("User flipped queen" = Cards[cardId].cardImage)
+}
+var cardsInPlay = ["king"]
+cardsInPlay.push ("cardTwo");
+cardsInPlay;
+console.log("User flipped king" = Cards[cardId].rank)
+console.log("User flipped king" = Cards[cardId].suit)
+console.log("User flipped king" = Cards[cardId].cardImage)
+
+
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen " + cardOne);
+
+
+var cardTwo = cards[0];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king " + cardTwo);
+
+
+createBoard {
+	for (var i = 0; i < createBoard.length; i++);
+	var cardElement = document.createElement('img');
+
+}
+cardElement.setAttribute('src', 'images/back.png');
+cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click')
+cardElement.appendChild('game-board')
